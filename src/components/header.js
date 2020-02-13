4fb94b72b894f7d7c5848cf8a9777012c81ecee8
Brent <barias@axiscode.com>
2020-02-12 18:45:15 -0700

    Initial Avatar Assembly Content.

--- src/components/header.js
+++ src/components/header.js
@@ -12,27 +12,65 @@ export default class Header extends React.Component {
   render() {
     return (
       <>
-        <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav" expand="lg"
-                collapseOnSelect={true}>
+        <Navbar
+          className="navbar navbar-expand-lg navbar-light fixed-top py-3"
+          id="mainNav"
+          expand="lg"
+          collapseOnSelect={true}
+        >
           <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}>Start Bootstrap</a>
-            <Navbar.Toggle aria-controls="navbarResponsive"/>
+            <a
+              className="navbar-brand js-scroll-trigger"
+              href="#page-top"
+              onClick={Scroller.handleAnchorScroll}
+            >
+              The Avatar Assembly
+            </a>
+            <Navbar.Toggle aria-controls="navbarResponsive" />
             <Navbar.Collapse id="navbarResponsive">
               <Nav className="navbar-nav ml-auto my-2 my-lg-0">
-                <Scrollspy className="navbar-nav"
-                           items={["about", "services", "portfolio", "contact"]}
-                           currentClassName="active" rootEl={"#mainNav"} offset={-75}>
+                <Scrollspy
+                  className="navbar-nav"
+                  items={["about", "elements", "portfolio", "contact"]}
+                  currentClassName="active"
+                  rootEl={"#mainNav"}
+                  offset={-75}
+                >
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>About</Nav.Link>
+                    <Nav.Link
+                      className={"js-scroll-trigger"}
+                      href="#about"
+                      onClick={Scroller.handleAnchorScroll}
+                    >
+                      About
+                    </Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Services</Nav.Link>
+                    <Nav.Link
+                      className={"js-scroll-trigger"}
+                      href="#elements"
+                      onClick={Scroller.handleAnchorScroll}
+                    >
+                      Elements
+                    </Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Portfolio</Nav.Link>
+                    <Nav.Link
+                      className={"js-scroll-trigger"}
+                      href="#portfolio"
+                      onClick={Scroller.handleAnchorScroll}
+                    >
+                      Portfolio
+                    </Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Contact</Nav.Link>
+                    <Nav.Link
+                      className={"js-scroll-trigger"}
+                      href="#contact"
+                      onClick={Scroller.handleAnchorScroll}
+                    >
+                      Contact
+                    </Nav.Link>
                   </li>
                 </Scrollspy>
               </Nav>
@@ -42,20 +80,29 @@ export default class Header extends React.Component {
         <header className="masthead">
           <div className="container h-100">
             <div className="row h-100 align-items-center justify-content-center text-center">
-              <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
-                  Themes</h1>
-                <hr className="divider my-4"/>
+              <div className="col-lg-12 align-self-end">
+                <h1 className="text-uppercase text-white font-weight-bold">
+                  Holistic wellness through food, lifestyle and connection
+                </h1>
+                <hr className="divider my-4" />
               </div>
-              <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
+              <div className="col-lg-6 align-self-baseline">
+                <p className="text-white-75 font-weight-light mb-5">
+                  Make a difference in your own life. Make a difference for
+                  others. Train, grow and lead with the power of community.{" "}
+                </p>
+                <a
+                  className="btn btn-primary btn-xl js-scroll-trigger"
+                  href="#about"
+                  onClick={Scroller.handleAnchorScroll}
+                >
+                  Find Out More
+                </a>
               </div>
             </div>
           </div>
         </header>
       </>
-    );
+    )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -35,151 +35,135 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="Home"/>
+        <SEO title="Home" />
         <section className="page-section bg-primary" id="about">
           <div className="container">
             <div className="row justify-content-center">
+              <div className="col-lg-6 text-center">
+                <h2 className="text-white mt-0">
+                  Find and step into your purpose.
+                </h2>
+                <hr className="divider light my-4" />
+                <p className="text-white-50 mb-4">
+                  The Avatar Assembly are the leaders whose light and talents
+                  brings healing to self, relationships, and the many. We
+                  restore, cherish, and master practices that have brought
+                  wellness for thousands of years. Our community finds joy in
+                  whole food, dance, song, music, and a sea of healing arts. And
+                  from our strength and love, we reach out to and invite others
+                  also seeking wellness and higher purpose.{" "}
+                </p>
+              </div>
+              <div className="col-lg-6 text-center">
+                <h2 className="text-white mt-0">
+                  Heal the body. Heal the mind.
+                </h2>
+                <hr className="divider light my-4" />
+                <p className="text-white-50 mb-4">
+                  The fullness of human potential is yours if you make it your
+                  dream to manifest your heart and spirit. Join with the Avatar
+                  Assembly - we who have made it our passion to live in harmony
+                  with ourselves, nature, and our fellow man. Together we share
+                  our human experiences, whether in joy or grief, where we learn
+                  and grow. United, we work and serve the needs of our community
+                  and extend our arm beyond to the world in need.{" "}
+                </p>
+              </div>
+            </div>
+            <hr className="divider my-4" />
+            <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
-                <h2 className="text-white mt-0">We've got what you need!</h2>
-                <hr className="divider light my-4"/>
-                <p className="text-white-50 mb-4">Start Bootstrap has everything you need to get your new website up
-                  and
-                  running in no time! Choose one of our open source, free to download, and easy to use themes! No
-                  strings
-                  attached!</p>
-                <a className="btn btn-light btn-xl js-scroll-trigger" href="#services"
-                   onClick={Scroller.handleAnchorScroll}>Get Started!</a>
+                <h2 className="text-white mt-0">
+                  Finding the Avatar Assembly.
+                </h2>
+                <hr className="divider light my-4" />
+                <p className="text-white-50 mb-4">
+                  The journey begins with a six hour Saturday seminar - the
+                  Avatar Integration. The seminar is a grounding of knowledge
+                  and experience in diet, lifestyle and self-awareness. The
+                  graduates from the Avatar Integration are then eligible to
+                  join or form micro-communities, each an Avatar Circle. Each
+                  Circle meets regularly at a natural cadence (from monthly, to
+                  weekly) and are the venue for true food, beauty (music, song,
+                  dance) and healing modalities. Each Circle then gathers its
+                  strength to deliver with power to a worthy public cause; the
+                  good that we have we must share as a light to others.
+                </p>
               </div>
             </div>
           </div>
         </section>
 
-        <section className="page-section" id="services">
+        <section className="page-section" id="elements">
           <div className="container">
-            <h2 className="text-center mt-0">At Your Service</h2>
-            <hr className="divider my-4"/>
+            <h2 className="text-center mt-0">
+              Reclaiming heritage. Creating legacy.
+            </h2>
+            <hr className="divider my-4" />
             <div className="row">
               <div className="col-lg-3 col-md-6 text-center">
-                <div className="mt-5">
-                  <i className="fas fa-4x fa-gem text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Sturdy Themes</h3>
-                  <p className="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+                <div className="mt-2">
+                  <i className="fas fa-4x fa-carrot text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Food is Thy Medicine</h3>
+                  <p className="text-muted mb-0">
+                    To be living with power, we must be well. Healing the mind
+                    and body begins with eating traditional whole foods. For us,
+                    preparing and sharing meals is both a meditation and
+                    powerful act of fellowship.
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
-                <div className="mt-5">
-                  <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Up to Date</h3>
-                  <p className="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+                <div className="mt-2">
+                  <i className="fas fa-4x fa-diagnoses text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Embrace Healing Arts</h3>
+                  <p className="text-muted mb-0">
+                    The Assembly honors and shares all forms of healing:
+                    kinesiology, acupuncture, meditation, Tai Chi, yoga, Reiki,
+                    chiropracty, massage, etcetera. Gifts of healing offered
+                    when we gather as a community strengthen our human bonds and
+                    gratitude.
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
-                <div className="mt-5">
-                  <i className="fas fa-4x fa-globe text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Ready to Publish</h3>
-                  <p className="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+                <div className="mt-2">
+                  <i className="fas fa-4x fa-ankh text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">A Life of Harmony</h3>
+                  <p className="text-muted mb-0">
+                    To have balance, we must honor our human nature. The
+                    Assembly makes time for ceremonial, ancestral, and fine arts
+                    which free the mind and spirit. Likewise we celebrate and
+                    make time for the outdoors, living as stewards of the earth.
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
-                <div className="mt-5">
-                  <i className="fas fa-4x fa-heart text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Made with Love</h3>
-                  <p className="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                <div className="mt-2">
+                  <i className="fas fa-4x fa-fire text-primary mb-4"></i>
+                  <h3 className="h4 mb-2">Passion & Power</h3>
+                  <p className="text-muted mb-0">
+                    The investment into our own well-being is the catalyst for
+                    the transcendence we desire. Each of us can touch positively
+                    thousands, and yet together we witness the whole is greater
+                    than the sum of the parts. What we hold as our intention, we
+                    accomplish!
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </section>
 
-        <section id="portfolio">
-          <div className="container-fluid p-0">
-            <div className="row no-gutters">
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg" onClick={this.handlePortfolioClick.bind(this, 0)}>
-                  <Img fluid={this.props.data.images.edges[0].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg" onClick={this.handlePortfolioClick.bind(this, 1)}>
-                  <Img fluid={this.props.data.images.edges[1].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg" onClick={this.handlePortfolioClick.bind(this, 2)}>
-                  <Img fluid={this.props.data.images.edges[2].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="images/portfolio/fullsize/4.jpg" onClick={this.handlePortfolioClick.bind(this, 3)}>
-                  <Img fluid={this.props.data.images.edges[3].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/5.jpg" onClick={this.handlePortfolioClick.bind(this, 4)}>
-                  <Img fluid={this.props.data.images.edges[4].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg" onClick={this.handlePortfolioClick.bind(this, 5)}>
-                  <Img fluid={this.props.data.images.edges[5].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption p-3">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className="page-section bg-dark text-white">
+        <section className="page-section bg-dark text-white" id="portfolio">
           <div className="container text-center">
-            <h2 className="mb-4">Free Download at Start Bootstrap!</h2>
-            <a className="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Download Now!</a>
+            <h2 className="mb-4">Make today the day your life takes a new direction.</h2>
+            <a
+              className="btn btn-light btn-xl"
+              href="https://startbootstrap.com/themes/creative/"
+            >
+              Contact Us
+            </a>
           </div>
         </section>
 
@@ -188,26 +172,36 @@ export default class IndexPage extends React.Component {
             <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
                 <h2 className="mt-0">Let's Get In Touch!</h2>
-                <hr className="divider my-4"/>
-                <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an
-                  email
-                  and we will get back to you as soon as possible!</p>
+                <hr className="divider my-4" />
+                <p className="text-muted mb-5">
+                  Ready to start a new future with us? Give us a call or
+                  send us an email and we will get back to you as soon as
+                  possible!
+                </p>
               </div>
             </div>
             <div className="row">
               <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                 <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
-                <div>+1 (202) 555-0149</div>
+                <div>(602) 692 8088</div>
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                <a className="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                <a className="d-block" href="mailto:contact@yourwebsite.com">
+                  contact@yourwebsite.com
+                </a>
               </div>
             </div>
           </div>
         </section>
-        <PortfolioModal show={this.state.modalShow} onHide={() => this.setModal(false, 0)}>
-          <PortfolioCarousel images={this.props.data.images.edges} current={this.state.modalCurrent}/>
+        <PortfolioModal
+          show={this.state.modalShow}
+          onHide={() => this.setModal(false, 0)}
+        >
+          <PortfolioCarousel
+            images={this.props.data.images.edges}
+            current={this.state.modalCurrent}
+          />
         </PortfolioModal>
       </Layout>
     )
