0afeb683b74762653a32dff471954c18e3e69384
Brent <barias@axiscode.com>
2020-03-03 13:37:01 -0700

    Add Google Analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,33 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      //FULL LIST OF OPTIONS
+      //https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#create
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-159564828-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: false,
+
+        // Avoids sending pageview hits from custom paths
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        //pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        siteSpeedSampleRate: 25,
+      },
+    },    
   ],
 }
